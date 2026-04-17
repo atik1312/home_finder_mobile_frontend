5f13a6040b7a9ac65ea2c38f7af122e384205a3c
atik1312 <shariaratik95@gmail.com>
2026-04-17 15:49:41 +0600

    edit post update

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -52,174 +52,177 @@ class _HomePageState extends State<HomePage> {
   }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Home Finder"),
-       centerTitle: true,
-       leading: null,
-        elevation: 2,
-        
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Color.fromARGB(255, 235, 245, 192),
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  CircleAvatar(
-                    radius: 30,
-                    backgroundImage: NetworkImage(getUserProfileImage()!),
-                  ),
-                  SizedBox(height: 10),
-                  Text(
-                    widget.user.name!,
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                  ),
-                   Text(
-                    widget.user.email,
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold,
-                    overflow: TextOverflow.ellipsis,
-                    ),
-                  ),
-                ],
-              )
-            ),
-            ListTile(
-              leading: Icon(Icons.home),
-              title: Text('All Homes'),
-              onTap: () async{
-                EasyLoading.show(status: "Loading...");
-                posts=await ApiService.posts( "all", null) as List<Post>;
-                setState(() {
-                });
-                EasyLoading.dismiss();
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.male_rounded),
-              title: Text('Boys houses'),
-              onTap: () async{
-                EasyLoading.show(status: "Loading...");
-                posts=await ApiService.posts( "male", null) as List<Post>;
-                log(posts.length.toString());
-                setState(() {
-                });
-                EasyLoading.dismiss();
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.female_rounded),
-              title: Text('Girls houses'),
-              onTap: ()async {
-                EasyLoading.show(status: "Loading...");
-                posts=await ApiService.posts( "female", null) as List<Post>;
-                setState(() {
-                });
-                EasyLoading.dismiss();
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.post_add_outlined),
-              title: Text('My Post'),
-              onTap: () {
-
-                Navigator.of(context).push(MaterialPageRoute(builder: (context)=>MyPosts(posts: posts, onDelete: onDelete,)));
-                
-              },
-            ),
-             ListTile(
-              leading: Icon(Icons.add),
-              title: Text('Create Post'),
-              onTap: () {
-                Navigator.pop(context);
-                context.push(CreatePost.routeName);
-              },
-            ),
-               ListTile(
-              leading: Icon(Icons.edit),
-              title: Text('Edit Profile'),
-              onTap: () {
-                Navigator.pop(context);
-                Navigator.of(context).push(MaterialPageRoute(builder: (context)=>EditProfile()));
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.logout),
-              title: Text('Logout'),
-              onTap: () {
-                LoggedUser.instance.clearUser();
-                context.go("/login");
-              },
-            ),
-          ],
+    return PopScope(
+      canPop: false,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Home Finder"),
+         centerTitle: true,
+         leading: null,
+          elevation: 2,
+          
         ),
-      ),
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-           
-            Padding(
-              padding: const EdgeInsets.all(15),
-              child: TextField(
-                controller: _searchController,
-                decoration: InputDecoration(
-                  hintText: "Search for homes",
-                  prefixIcon: IconButton(onPressed: ()async {
-                    final query = _searchController.text;
-                    EasyLoading.show(status: "Searching...");
-                    posts = await ApiService.posts("all", query) as List<Post>;
-                    setState(() {});
-                    EasyLoading.dismiss();
-
-                  }, icon: Icon(Icons.search)),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15),
-                    borderSide: BorderSide.none,
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 235, 245, 192),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    CircleAvatar(
+                      radius: 30,
+                      backgroundImage: NetworkImage(getUserProfileImage()!),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      widget.user.name!,
+                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                     Text(
+                      widget.user.email,
+                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold,
+                      overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ],
+                )
+              ),
+              ListTile(
+                leading: Icon(Icons.home),
+                title: Text('All Homes'),
+                onTap: () async{
+                  EasyLoading.show(status: "Loading...");
+                  posts=await ApiService.posts( "all", null) as List<Post>;
+                  setState(() {
+                  });
+                  EasyLoading.dismiss();
+                  context.pop();
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.male_rounded),
+                title: Text('Boys houses'),
+                onTap: () async{
+                  EasyLoading.show(status: "Loading...");
+                  posts=await ApiService.posts( "male", null) as List<Post>;
+                  log(posts.length.toString());
+                  setState(() {
+                  });
+                  EasyLoading.dismiss();
+                  context.pop();
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.female_rounded),
+                title: Text('Girls houses'),
+                onTap: ()async {
+                  EasyLoading.show(status: "Loading...");
+                  posts=await ApiService.posts( "female", null) as List<Post>;
+                  setState(() {
+                  });
+                  EasyLoading.dismiss();
+                  context.pop();
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.post_add_outlined),
+                title: Text('My Post'),
+                onTap: () {
+      
+                  Navigator.of(context).push(MaterialPageRoute(builder: (context)=>MyPosts(posts: posts, onDelete: onDelete,)));
+                  
+                },
+              ),
+               ListTile(
+                leading: Icon(Icons.add),
+                title: Text('Create Post'),
+                onTap: () {
+                  context.pop();
+                  context.push(CreatePost.routeName);
+                },
+              ),
+                 ListTile(
+                leading: Icon(Icons.edit),
+                title: Text('Edit Profile'),
+                onTap: () {
+                  context.pop();
+                  Navigator.of(context).push(MaterialPageRoute(builder: (context)=>EditProfile()));
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.logout),
+                title: Text('Logout'),
+                onTap: () {
+                  LoggedUser.instance.clearUser();
+                  context.go("/login");
+                },
+              ),
+            ],
+          ),
+        ),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+             
+              Padding(
+                padding: const EdgeInsets.all(15),
+                child: TextField(
+                  controller: _searchController,
+                  decoration: InputDecoration(
+                    hintText: "Search for homes",
+                    prefixIcon: IconButton(onPressed: ()async {
+                      final query = _searchController.text;
+                      EasyLoading.show(status: "Searching...");
+                      posts = await ApiService.posts("all", query) as List<Post>;
+                      setState(() {});
+                      EasyLoading.dismiss();
+      
+                    }, icon: Icon(Icons.search)),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      borderSide: BorderSide.none,
+                    ),
+                    filled: true,
+                    fillColor: Color.fromARGB(255, 235, 245, 192),
                   ),
-                  filled: true,
-                  fillColor: Color.fromARGB(255, 235, 245, 192),
                 ),
               ),
-            ),
-            Expanded(
-              child: RefreshIndicator(
-                onRefresh: fetchPosts,
-                child: posts.length>0?ListView.builder(
-                  physics: const AlwaysScrollableScrollPhysics(),
-                  itemCount: posts.length,
-                  itemBuilder: (context, index) {
-                    log(posts[index].toString());
-                    final post = posts[index];
-                    if(post.user==widget.user.userId|| post.isRented!){
-                      return SizedBox.shrink();
-                    }
-                    log(index.toString());
-                    return PostItem(post: post);
-                  },
-                ): Center(child: Text("No posts found")),
+              Expanded(
+                child: RefreshIndicator(
+                  onRefresh: fetchPosts,
+                  child: posts.length>0?ListView.builder(
+                    physics: const AlwaysScrollableScrollPhysics(),
+                    itemCount: posts.length,
+                    itemBuilder: (context, index) {
+                      log(posts[index].toString());
+                      final post = posts[index];
+                      if(post.user==widget.user.userId|| post.isRented!){
+                        return SizedBox.shrink();
+                      }
+                      log(index.toString());
+                      return PostItem(post: post);
+                    },
+                  ): Center(child: Text("No posts found")),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
+      
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            context.push(CreatePost.routeName);
+          },
+          backgroundColor: Colors.transparent,
+          child: Icon(Icons.add,color: Colors.white,),
+        ),
+       
       ),
-
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          context.push(CreatePost.routeName);
-        },
-        backgroundColor: Colors.transparent,
-        child: Icon(Icons.add,color: Colors.white,),
-      ),
-     
     );
   }
 }
--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -66,145 +66,148 @@ class _LoginPageState extends State<LoginPage> with SingleTickerProviderStateMix
   @override
   Widget build(BuildContext context) {
     final size=MediaQuery.of(context).size;
-    return Scaffold(
-      // resizeToAvoidBottomInset: false,
-      body: Container(
-        height: double.infinity,
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [const Color.fromARGB(255, 239, 235, 247),const Color.fromARGB(255, 211, 202, 245)],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight
-          )
-        ),
-        child: SingleChildScrollView(
-          child: Column(
-            children:[
-            SizedBox(height: size.height*0.05,),
-            Lottie.asset("assets/loginLogo.json",width: size.width*0.7,height: size.height*0.35,
-            repeat: true
-            ),
-            AnimatedOpacity(
-              duration: const Duration(seconds: 1),
-              opacity: _textOpacity,
-              child: Text("Home Finder!",style: TextStyle(fontSize: 35,fontWeight: FontWeight.bold,color: Colors.black),)),
-            SizedBox(height: size.height*0.01,),
-            SlideTransition(
-              position: _animation,
-              child: Container(
-                width: double.infinity,
-                padding: const EdgeInsets.all(16.0),
-                margin: const EdgeInsets.all(24.0),
-                decoration: BoxDecoration(
-                  // color: Colors.white,
-                  gradient: LinearGradient(
-                    colors: [const Color.fromARGB(255, 255, 255, 255),const Color.fromARGB(255, 255, 255, 255)],
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight
+    return PopScope(
+      canPop: false,
+      child: Scaffold(
+        // resizeToAvoidBottomInset: false,
+        body: Container(
+          height: double.infinity,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [const Color.fromARGB(255, 239, 235, 247),const Color.fromARGB(255, 211, 202, 245)],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight
+            )
+          ),
+          child: SingleChildScrollView(
+            child: Column(
+              children:[
+              SizedBox(height: size.height*0.05,),
+              Lottie.asset("assets/loginLogo.json",width: size.width*0.7,height: size.height*0.35,
+              repeat: true
+              ),
+              AnimatedOpacity(
+                duration: const Duration(seconds: 1),
+                opacity: _textOpacity,
+                child: Text("Home Finder!",style: TextStyle(fontSize: 35,fontWeight: FontWeight.bold,color: Colors.black),)),
+              SizedBox(height: size.height*0.01,),
+              SlideTransition(
+                position: _animation,
+                child: Container(
+                  width: double.infinity,
+                  padding: const EdgeInsets.all(16.0),
+                  margin: const EdgeInsets.all(24.0),
+                  decoration: BoxDecoration(
+                    // color: Colors.white,
+                    gradient: LinearGradient(
+                      colors: [const Color.fromARGB(255, 255, 255, 255),const Color.fromARGB(255, 255, 255, 255)],
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight
+                    ),
+                    borderRadius: BorderRadius.circular(20.0)
                   ),
-                  borderRadius: BorderRadius.circular(20.0)
-                ),
-                child: Form(
-                  key: _formKey,
-                  child:ListView(
-                    padding: const EdgeInsets.all(10.0),
-                    shrinkWrap: true,
-                    children: [
-                   
-                      Padding(
-                        padding: const EdgeInsets.all(4.0),
-                        child: TextFormField(
-                          controller: _emailController,
-                          keyboardType:TextInputType.emailAddress ,
-                          decoration: InputDecoration(
-                            prefixIcon: const Icon(Icons.email),
-                            hintText: "Email Address",
-                            filled: true,
-                            fillColor: const Color.fromARGB(255, 235, 245, 192),
-                            border: OutlineInputBorder(
-                              borderSide: BorderSide.none,
+                  child: Form(
+                    key: _formKey,
+                    child:ListView(
+                      padding: const EdgeInsets.all(10.0),
+                      shrinkWrap: true,
+                      children: [
+                     
+                        Padding(
+                          padding: const EdgeInsets.all(4.0),
+                          child: TextFormField(
+                            controller: _emailController,
+                            keyboardType:TextInputType.emailAddress ,
+                            decoration: InputDecoration(
+                              prefixIcon: const Icon(Icons.email),
+                              hintText: "Email Address",
+                              filled: true,
+                              fillColor: const Color.fromARGB(255, 235, 245, 192),
+                              border: OutlineInputBorder(
+                                borderSide: BorderSide.none,
+                                borderRadius: BorderRadius.circular(8.0)
+                              )
+                            ),
+                            validator: (value) {
+                              if(value==null|| value.isEmpty){
+                                return 'Provide a valid email address';
+                              }
+                              return null;
+                            },
+                          ),
+                        ),
+                        SizedBox(height: 8.0,),
+                        Padding(
+                          padding: const EdgeInsets.all(4.0),
+                          child: TextFormField(
+                            obscureText: _isSecured,
+                            controller: _passwordController,
+                            decoration: InputDecoration(
+                              filled: true,
+                              prefixIcon: const Icon(Icons.password),
+                              suffixIcon: IconButton(onPressed: (){
+                                setState(() {
+                                  _isSecured=!_isSecured;
+                                });
+                              }, icon: Icon(_isSecured?Icons.visibility:Icons.visibility_off)),
+                              hintText: "Password (atleast 6 characters)",
+                               fillColor: const Color.fromARGB(255, 235, 245, 192),
+                              border: OutlineInputBorder(
+                                borderSide: BorderSide.none,
+                                borderRadius: BorderRadius.circular(8.0)
+                              )
+                            ),
+                            validator: (value) {
+                              if(value==null|| value.isEmpty||value.length<6){
+                                return 'Provide a valid password';
+                              }
+                              return null;
+                            },
+                          ),
+                        ),
+                        SizedBox(height: 8.0,),
+                        Padding(padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width*0.25),
+                        child: ElevatedButton(onPressed:_authenticate,
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: const Color.fromARGB(255, 243, 247, 198),
+                            foregroundColor: Colors.black,  
+                            elevation: 10,
+                            shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(8.0)
                             )
                           ),
-                          validator: (value) {
-                            if(value==null|| value.isEmpty){
-                              return 'Provide a valid email address';
-                            }
-                            return null;
-                          },
+                          child: Text("Login")),
                         ),
-                      ),
-                      SizedBox(height: 8.0,),
-                      Padding(
-                        padding: const EdgeInsets.all(4.0),
-                        child: TextFormField(
-                          obscureText: _isSecured,
-                          controller: _passwordController,
-                          decoration: InputDecoration(
-                            filled: true,
-                            prefixIcon: const Icon(Icons.password),
-                            suffixIcon: IconButton(onPressed: (){
-                              setState(() {
-                                _isSecured=!_isSecured;
-                              });
-                            }, icon: Icon(_isSecured?Icons.visibility:Icons.visibility_off)),
-                            hintText: "Password (atleast 6 characters)",
-                             fillColor: const Color.fromARGB(255, 235, 245, 192),
-                            border: OutlineInputBorder(
-                              borderSide: BorderSide.none,
-                              borderRadius: BorderRadius.circular(8.0)
-                            )
-                          ),
-                          validator: (value) {
-                            if(value==null|| value.isEmpty||value.length<6){
-                              return 'Provide a valid password';
-                            }
-                            return null;
-                          },
-                        ),
-                      ),
-                      SizedBox(height: 8.0,),
-                      Padding(padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width*0.25),
-                      child: ElevatedButton(onPressed:_authenticate,
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: const Color.fromARGB(255, 243, 247, 198),
-                          foregroundColor: Colors.black,  
-                          elevation: 10,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8.0)
+                        SizedBox(height: 8.0,),
+                        
+                        SizedBox(height: 8.0,),
+                        RichText(
+                          textAlign: TextAlign.center,
+                          text: TextSpan(
+                            text: "Don't have an account? ",
+                            style: TextStyle(color: Colors.black),
+                            children: [
+                              TextSpan(
+                                text: "Sign Up",
+                                style: TextStyle(color: Colors.blue,fontWeight: FontWeight.bold),
+                                recognizer: TapGestureRecognizer()..onTap=(){
+                                  context.goNamed(SignUp.routeName);
+                                }
+                              )
+                            ]
                           )
                         ),
-                        child: Text("Login")),
-                      ),
-                      SizedBox(height: 8.0,),
-                      
-                      SizedBox(height: 8.0,),
-                      RichText(
-                        textAlign: TextAlign.center,
-                        text: TextSpan(
-                          text: "Don't have an account? ",
-                          style: TextStyle(color: Colors.black),
-                          children: [
-                            TextSpan(
-                              text: "Sign Up",
-                              style: TextStyle(color: Colors.blue,fontWeight: FontWeight.bold),
-                              recognizer: TapGestureRecognizer()..onTap=(){
-                                context.goNamed(SignUp.routeName);
-                              }
-                            )
-                          ]
-                        )
-                      ),
-                      _errMsg.isNotEmpty? Text(_errMsg,style: TextStyle(fontSize: 18,color: Colors.red),):SizedBox.shrink()
-                      
-                    ],
-                  )
-                  ),
+                        _errMsg.isNotEmpty? Text(_errMsg,style: TextStyle(fontSize: 18,color: Colors.red),):SizedBox.shrink()
+                        
+                      ],
+                    )
+                    ),
+                ),
               ),
+              ]
             ),
-            ]
-          ),
-        )
+          )
+        ),
       ),
     );
  
